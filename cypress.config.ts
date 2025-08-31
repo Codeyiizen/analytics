4d9a91599c96bbdac2e3e9bcb94337376b9ab01a
maneeshtiwari007 <maneeshtiwari007@gmail.com>
2025-08-31 21:56:04 +0530

    commit changes
    commit changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   // default username / password on init
   env: {
     umami_user: 'admin',
-    umami_password: 'umami',
+    umami_password: 'code@123',
     umami_user_id: '41e2b680-648e-4b09-bcd7-3e2b10c06264',
   },
 });
